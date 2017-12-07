3ebafe9acf0f95e8a70e95ad7ca7fdfb2534e459
Zachary Klein <zak@silver-chalice.com>
2017-12-07 10:09:39 -0500

    Added Okta starter & Spring Security OAuth2

--- server/grails-app/init/grailsreactexample/Application.groovy
+++ server/grails-app/init/grailsreactexample/Application.groovy
@@ -2,7 +2,9 @@ package grailsreactexample
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
 
+@EnableResourceServer
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
