7a73cb444a02fdb8c2b2017253ae1ece8cdea996
Zachary Klein <zak@silver-chalice.com>
2017-12-07 10:35:50 -0500

    added wrapper

---
+++ server/src/test/groovy/com/example/demo/beer/BeerRestfulControllerSpec.groovy
@@ -0,0 +1,18 @@
+package com.example.demo.beer
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class BeerRestfulControllerSpec extends Specification implements ControllerUnitTest<BeerRestfulController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+        true == false
+    }
+}
---
+++ server/src/test/groovy/com/example/demo/beer/BeerSpec.groovy
@@ -0,0 +1,18 @@
+package com.example.demo.beer
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class BeerSpec extends Specification implements DomainUnitTest<Beer> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
